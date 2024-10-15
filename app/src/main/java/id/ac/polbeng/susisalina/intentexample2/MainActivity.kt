c5045b3ca8bb8303c07249b385071135af4a3ba3
susi <susisalina903@gmail.com>
2024-10-15 14:33:39 +0700

    susi salina

---
+++ app/src/main/java/id/ac/polbeng/susisalina/intentexample2/MainActivity.kt
@@ -0,0 +1,22 @@
+package id.ac.polbeng.susisalina.intentexample2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.polbeng.susisalina.intentexample2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            val sendData = binding.editText.text.toString()
+            val intent = Intent(this@MainActivity,
+                SecondActivity::class.java)
+            intent.putExtra("main_activity_data", sendData)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/id/ac/polbeng/susisalina/intentexample2/SecondActivity.kt
@@ -0,0 +1,16 @@
+package id.ac.polbeng.susisalina.intentexample2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.polbeng.susisalina.intentexample2.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val receiveData = intent.getStringExtra("main_activity_data")
+        binding.textView.text = receiveData
+    }
+}
